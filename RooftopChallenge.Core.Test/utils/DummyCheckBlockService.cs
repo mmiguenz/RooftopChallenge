2c5bd6c08082958e559ac3a11886bac5f6929230
Matias <miguenz.matias@gmail.com>
2022-06-25 18:52:48 -0300

    adds missing files

---
+++ RooftopChallenge.Core.Test/utils/DummyCheckBlockService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using RooftopChallenge.Core.Domain;
+
+namespace RooftopChallenge.Core.Test.utils
+{
+    public class DummyCheckBlockService : ICheckBlockService
+    {
+        private readonly string _orderedBlocks;
+        public int _calls = 0;
+
+        public DummyCheckBlockService(List<String> orderedBlocks)
+        {
+            _orderedBlocks = String.Join("", orderedBlocks);
+        }
+
+        public bool AreConsequent(ImmutableList<string> list)
+        {
+            _calls++;
+            var input = string.Join("", list);
+
+            return _orderedBlocks.Contains(input);
+        }
+
+
+    }
+}
---
+++ RooftopChallenge.Core.Test/utils/Helper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace RooftopChallenge.Core.Test.utils
+{
+    public class Helper
+    {
+        public static IEnumerable<string> Shuffle(IEnumerable<string> orderedBlocks)
+        {
+            RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
+            int n = orderedBlocks.Count();
+            var shuffledList = orderedBlocks.ToList();
+            while (n > 1)
+            {
+                byte[] box = new byte[1];
+                do provider.GetBytes(box);
+                while (!(box[0] < n * (Byte.MaxValue / n)));
+                int k = (box[0] % n);
+                n--;
+                string value = shuffledList[k];
+                shuffledList[k] = shuffledList[n];
+                shuffledList[n] = value;
+            }
+
+            return shuffledList;
+        }
+    }
+}
